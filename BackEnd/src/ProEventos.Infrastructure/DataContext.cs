f8647ba018eb40ec62a63dc13abf5c742de59000
Mario Gomes <mario.gomes@bravantic.co.ao>
2023-08-07 15:27:01 +0100

    Update ConnectionString

--- BackEnd/src/ProEventos.Infrastructure/DataContext.cs
+++ BackEnd/src/ProEventos.Infrastructure/DataContext.cs
@@ -3,10 +3,14 @@ using ProEventos.Domain.Entities;
 
 namespace ProEventos.Infrastructure
 {
-    public class DataContext:DbContext
+    public class DataContext: DbContext
     {
+        public DataContext() { }
+
+        public DataContext(DbContextOptions<DataContext> options): base(options) { }
+
         public DbSet<Evento> Eventos { get; set; }
-        public DataContext(DbContextOptions<DataContext> options):base(options){}
+     
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
